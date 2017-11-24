0d22765c9e00963451931b3ea2b13bd4cd49add7
Coredeveloper1988 <33131161+Coredeveloper1988@users.noreply.github.com>
2017-11-24 17:19:19 +0200

    update db

--- inc/db.php
+++ inc/db.php
@@ -4,11 +4,11 @@ include('inc/settings.php');
 
 try
 {
-    // $con = new PDO('mysql:host=' . $config['HOST'] . ';dbname=' . $config['DB'] . ';charset=UTF8', $config['USER'], $config['PASS']);
+    $con = new PDO('mysql:host=' . $config['HOST'] . ';dbname=' . $config['DB'] . ';charset=UTF8', $config['USER'], $config['PASS']);
 }
 catch(PDOException $e)
 {
-    // die($e->getMessage());
+    die($e->getMessage());
 }
 
 ?>
